308f75db43b41418ad1670a990b16c9c2bcd851c
arthur-padua-menezes-martins <arthur.software.developer@gmail.com>
2020-11-25 07:14:57 -0300

    feat: apply effect in PageApp to send request

--- src/presentation/pages/App/App.tsx
+++ src/presentation/pages/App/App.tsx
@@ -1,16 +1,30 @@
-import React, { useState } from 'react'
-import PageSearch from '@/presentation/pages/Products/Products'
+import React, { useState, useEffect } from 'react'
+import PageProducts from '@/presentation/pages/Products/Products'
 import ComponentInitialTransition from '@/presentation/components/Transition/Initial/InitialTransition'
+import { AppController } from './script'
+import { RemoteSearch } from '@/data/usecases/http/remote/remote-search'
+import { FeatchHttpClient } from '@/infra/http/client/fetch/fetch-http-client'
 import { StyledApp } from './style'
+import env from '@/main/config/env'
+
+const appController = new AppController(new RemoteSearch(new FeatchHttpClient()))
 
 const App: React.FC = () => {
   const [transitionOnly, setTransitionOnly] = useState(true)
+  const [productsList, setProductsList] = useState({ productsList: [{}] })
+
+  useEffect(() => {
+    (async () => {
+      const productsList = await appController.search({ url: env.url, method: env.method })
+      productsList?.body && setProductsList(productsList.body)
+    })()
+  }, [])
 
   return (
     <StyledApp className="container">
       {transitionOnly && <ComponentInitialTransition setOnly={setTransitionOnly.bind(this)}/>}
 
-      {!transitionOnly && <PageSearch/>}
+      {!transitionOnly && <PageProducts productsList={productsList.productsList}/>}
     </StyledApp>
   )
 }
