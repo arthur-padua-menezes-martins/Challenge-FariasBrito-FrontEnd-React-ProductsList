c1f90559ea5bb4e65297f356363faea631b1f60e
Arthur Padua de Menezes Martins <arthur.software.developer@gmail.com>
2020-11-26 06:38:13 -0300

    feat: create ComponentCheckbox storage dependencies

---
+++ src/presentation/components/Checkbox/store/index.ts
@@ -0,0 +1,9 @@
+import { useDispatch } from 'react-redux'
+import {
+  actionsCartSelectProducts
+} from '@/redux/store/ProductsList/actions/cart/select/SelectProducts.actions'
+
+export {
+  useDispatch,
+  actionsCartSelectProducts
+}
