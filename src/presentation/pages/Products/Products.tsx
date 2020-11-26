8e6dce484b52ec503bf7589daf0e888b1bdc2a88
Arthur Padua de Menezes Martins <arthur.software.developer@gmail.com>
2020-11-26 06:42:53 -0300

    feat: implement product listing

--- src/presentation/pages/Products/Products.tsx
+++ src/presentation/pages/Products/Products.tsx
@@ -4,44 +4,62 @@ import {
   listReducers, cartReducers
 } from './store'
 import {
-  ComponentTable, ComponentTBody, ComponentTr, ComponentTh, ComponentTd
+  ComponentCheckbox, ComponentTable, ComponentTBody, ComponentTr, ComponentTh, ComponentTd
 } from './components'
-import { IPropsTypes } from './script'
 import {
   StyledPageProductsGridContainer, StyledPageProductsGridContent
 } from './style'
 
-const PageProducts: FC<IPropsTypes> = (props) => {
+const PageProducts: FC = () => {
   const list = useSelector(listReducers)
   const cart = useSelector(cartReducers)
 
-  const [productsList] = useState(props.productsList)
   const [offersContent, setOffersContent] = useState([undefined])
+  const [cartContent, setCartContent] = useState([undefined])
 
   useEffect(() => {
-    console.log('productsList: ', productsList)
     console.log('cart: ', cart)
     console.log('list: ', list)
-    console.log('list.length: ', list.length)
     const resultOffersContent: any[] = []
+    const offersRightWidth = '20%'
+    const offersLeftWidth = '80%'
 
     resultOffersContent.push(
-      <ComponentTr height={5}>
-        <ComponentTh width={'15%'}>adicionar</ComponentTh>
-        <ComponentTh width={'85%'}>produto / valor</ComponentTh>
+      <ComponentTr position={'fixed'} height={5} direction={'row'}>
+        <ComponentTh width={offersRightWidth}><span>adicionar</span></ComponentTh>
+        <ComponentTh width={offersLeftWidth}><span>produto / valor</span></ComponentTh>
       </ComponentTr>
     )
 
     for (let i = 0; i < list.length; i++) {
       resultOffersContent.push(
         <ComponentTr>
-          <ComponentTd width={'15%'}></ComponentTd>
-          <ComponentTd width={'85%'}></ComponentTd>
+          <ComponentTd width={offersRightWidth}>
+            <ComponentCheckbox references={list[i].id}/>
+          </ComponentTd>
+
+          <ComponentTd width={offersLeftWidth}>
+            <p>{list[i].name}</p>
+            <p>{list[i].description}</p>
+            <p>{list[i].price}</p>
+          </ComponentTd>
         </ComponentTr>
       )
     }
 
+    const resultCartContent: any[] = []
+    const cartRightWidth = '50%'
+    const cartLeftWidth = '50%'
+
+    resultCartContent.push(
+      <ComponentTr position={'fixed'} height={5}>
+        <ComponentTh width={cartRightWidth}><span>produto</span></ComponentTh>
+        <ComponentTh width={cartLeftWidth}><span>valor</span></ComponentTh>
+      </ComponentTr>
+    )
+
     setOffersContent(resultOffersContent)
+    setCartContent(resultCartContent)
   }, [])
 
   return (
@@ -64,6 +82,13 @@ const PageProducts: FC<IPropsTypes> = (props) => {
 
         <StyledPageProductsGridContent area={'cart'} className="flex-center">
           <article>Carrinho</article>
+
+          <ComponentTable>
+            <ComponentTBody>
+              {cartContent}
+            </ComponentTBody>
+          </ComponentTable>
+
         </StyledPageProductsGridContent>
 
       </StyledPageProductsGridContainer>
