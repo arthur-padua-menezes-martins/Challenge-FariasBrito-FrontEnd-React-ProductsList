6e2bcfa5946238741e418addd6a10b5787fae2f7
Arthur Padua de Menezes Martins <arthur.software.developer@gmail.com>
2020-11-26 06:36:30 -0300

    refactor: code refactor in ComponentTd

--- src/presentation/components/Td/Td.tsx
+++ src/presentation/components/Td/Td.tsx
@@ -5,7 +5,7 @@ import { StyledTd } from './style'
 const ComponentTd: FC<IPropsTypes> = ({ width, height, children }) => {
   return (
     <>
-      <StyledTd className="flex-center" width={width} height={height}>
+      <StyledTd className="flex flex-column align-start flex-align-center" width={width} height={height}>
         {children}
       </StyledTd>
     </>
--- src/presentation/components/Td/style.ts
+++ src/presentation/components/Td/style.ts
@@ -4,7 +4,31 @@ interface IStyledTdProps {
   width?: string
   height?: number
 }
-export const StyledTd = styled.td<IStyledTdProps>`
+export const StyledTd = styled.div<IStyledTdProps>`
+  justify-content: flex-start;
   width: ${props => props.width ?? '100%'};
   height: ${props => `${props.height}rem` ?? '100%'};
+
+  p {
+    display: flex;
+    font-size: 1.2rem;
+    align-items: center;
+  }
+
+  p:nth-child(1) {
+    height: 30%;
+    font-weight: 500;
+  }
+
+  p:nth-child(2) {
+    align-items: baseline;
+    height: 40%;
+    font-weight: 300;
+    color: ${props => props.theme.colors.graySecondary}
+  }
+
+  p:nth-child(3) {
+    height: 40%;
+    font-weight: 500;
+  }
 `
